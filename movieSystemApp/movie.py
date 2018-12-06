ba64f91354d9957d7c4b4e2239d2fa73ecd3a946
gareththomasnz <gareththomasnz@yahoo.com>
2018-12-06 15:49:06 +1300

    fourth commit

--- movieSystemApp/movie.py
+++ movieSystemApp/movie.py
@@ -4,7 +4,16 @@ class Movie:
         self.genre = genre
         self.watched = watched
 
-
     def __repr__(self):
         return "<Movie {}>".format(self.name)
 
+    def json(self):
+        return {
+            'name': self.name,
+            'genre': self.genre,
+            'watched': self.watched
+        }
+
+    @classmethod
+    def from_json(cls, json_data):  # {'genre': '...', 'name': '...', 'watched': True}
+        return Movie(**json_data)
--- movieSystemApp/user.py
+++ movieSystemApp/user.py
@@ -1,5 +1,6 @@
 from movieSystemApp.movie import Movie
 
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -8,40 +9,35 @@ class User:
     def __repr__(self):
         return "<User {}>".format(self.name)
 
-
-    def  add_movie(self, name, genre):
+    def add_movie(self, name, genre):
         movie = Movie(name, genre, False)
         self.movies.append(movie)
 
     def delete_movie(self, name):
         self.movies = list(filter(lambda movie: movie.name != name, self.movies))
 
-
     def watched_movies(self):
-        return list(filter(lambda movie : movie.watched, self.movies))
+        return list(filter(lambda movie: movie.watched, self.movies))
 
+    def set_watched(self, name):
+        for movie in self.movies:
+            if movie.name == name:
+                movie.watched = True
 
-    def save_to_file(self):
-        with open(self.name, 'w') as f:
-            f.write(self.name + "\n")
-            for movie in self.movies:
-                # f.write(self.movies[0].name + "," + self.movies[0].genre + "," + str(self.movies[0].watched))
-                f.write("{},{},{}\n".format(movie.name, movie.genre, str(movie.watched)))
+    def json(self):
+        return {
+            'name': self.name,
+            'movies': [
+                movie.json() for movie in self.movies
+            ]
+        }
 
     @classmethod
-    def load_from_file(cls, filename):
-        with open(filename, 'r') as f:
-            content = f.readlines()
-            username = content[0]
-            movies = []
-            for line in content[1:]:
-                movie_data = line.split(",")
-                movies.append(Movie(movie_data[0],movie_data[1], movie_data[2] == "True"))
+    def from_json(cls, json_data):
+        user = User(json_data['name'])
+        movies = []
+        for movie_data in json_data['movies']:
+            movies.append(Movie.from_json(movie_data))
+        user.movies = movies
 
-            user = cls(username)
-            user.movies = movies
-            return user
-
-
-
-
+        return user
